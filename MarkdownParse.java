b84afe723889b523c08ccf1b381c55780b68dc4b
s-rsunku <s-rsunku@lwsd.org>
2022-01-28 15:00:16 -0800

    Fixed negative index infinite loop

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,8 +13,14 @@ public class MarkdownParse {
         while(currentIndex < markdown.length()) {
             int nextOpenBracket = markdown.indexOf("[", currentIndex);
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
+            if(nextOpenBracket == -1 || nextCloseBracket == -1) {
+                break;
+            }
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            if(openParen == -1 || closeParen == -1) {
+                break;
+            }
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
